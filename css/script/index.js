971ed2f3d3d98c83367a9db7579214be837c5b82
asha mohamed <falastine924@gmail.com>
2025-06-01 16:05:48 +0300

    index.html

---
+++ css/script/index.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+  alert("Welcome to Asha's Website!");
+});
+
+document.getElementById("toggleMode").addEventListener("click", function() {
+  document.body.classList.toggle("dark-mode");
+});
